345acfbb7b9819acbdf569676f6709393e9ee86a
James Wilson <me@unbui.lt>
2016-12-23 18:32:54 +0000

    day20, midway through improving approach to be nice and quick in both cases

--- day20.hs
+++ day20.hs
@@ -26,7 +26,7 @@ loadInput = do
 
 compactRanges :: [Range] -> [Range]
 compactRanges [] = []
-compactRanges rs = loop rs []
+compactRanges rs = List.sort $ loop rs []
   where
     loop :: [Range] -> [Range] -> [Range]
     loop [] outRs = outRs
@@ -35,10 +35,15 @@ compactRanges rs = loop rs []
         in loop newRs (newR:outRs)
     compress :: Range -> [Range] -> [Range] -> (Range,[Range])
     compress r [] newRs = (r, newRs)
-    compress (lo1,hi1) ((lo2,hi2):ss) newRs =
-        if lo1 <= lo2 && hi1 >= lo2 then compress (lo1, max hi1 hi2) ss newRs
-        else if lo2 <= lo1 && hi2 >= lo1 then compress (lo2, max hi1 hi2) ss newRs
-        else compress (lo1,hi1) ss ((lo2,hi2):newRs)
+    compress r@(lo1,hi1) (s@(lo2,hi2):ss) newRs =
+        if isOverlap r s
+            then compress (min lo1 lo2, max hi1 hi2) ss newRs
+            else compress r ss (s:newRs)
+    isOverlap :: Range -> Range -> Bool
+    isOverlap (lo1,hi1) (lo2,hi2) =
+        if lo1 <= (hi2+1) && hi1 >= (lo2-1) then True
+        else if lo2 <= (hi1+1) && hi2 >= (lo1-1) then True
+        else False
 
 isInRanges :: [Range] -> Word32 -> Bool
 isInRanges rs w = List.any (\(lo,hi) -> w >= lo && w <= hi) rs
@@ -47,6 +52,8 @@ main :: IO ()
 main = do
 
     ranges <- fmap compactRanges loadInput
+
+    print ranges
 
     putStrLn "Star 1:"
     print $ head $ filter (not . isInRanges ranges) $ [0..]
